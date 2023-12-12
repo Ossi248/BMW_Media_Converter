21fe009e18473cccef42deae77f99259ed4cc000
LarsDerNorweger <boettger.colin@gmail.com>
2023-12-12 22:25:50 +0100

    fix format

--- src/converter/converter.py
+++ src/converter/converter.py
@@ -1,16 +1,16 @@
-from os import path , remove
-
+from os import path, remove
 
 extensionMap = {
-    ".mp3":".br4",
-    ".br4":".mp3"
+    ".mp3": ".br4",
+    ".br4": ".mp3"
 }
 
-def convert_file_bitwise(src:str, dest:str):
+
+def convert_file_bitwise(src: str, dest: str):
     if not path.exists(src):
         raise Exception(f"The File {src} does not exitst")
     try:
-        tar = open(dest,"+bw") 
+        tar = open(dest, "+bw")
         with open(src, "rb") as file:
             byte = file.read(1024)
             while byte:
@@ -23,12 +23,13 @@ def convert_file_bitwise(src:str, dest:str):
     except Exception as e:
         remove(dest)
         raise e
-    
-def convert_file(src:str, dest:str):
-    name,ext=path.splitext(src)
+
+
+def convert_file(src: str, dest: str):
+    name, ext = path.splitext(src)
     try:
         result = f"{dest}{path.basename(src).replace(ext,extensionMap[ext])}"
     except Exception as e:
         print(e)
         raise Exception(f"Unkown extension {ext}")
-    convert_file_bitwise(src,result)
+    convert_file_bitwise(src, result)
