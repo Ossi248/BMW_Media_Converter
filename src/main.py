7798c6bd5eab2bde7c6b662863ff7b2f6d4e1666
Ossi248 <oskar.wolny@gmail.com>
2023-12-14 14:57:26 +0100

    adding gui and adding workflow-file, change main

--- src/main.py
+++ src/main.py
@@ -6,9 +6,16 @@
 
 import converter
 
+from gui.gui import GUI
+
 
 def done():
     print("DONE")
 
 
-converter.convert_directory_parallel("./test", "./hallo", done, print, 4)
+def main():
+    gui = GUI('BR3/4/5 Converter', (500,400))
+    converter.convert_directory_parallel("./test", "./hallo", done, print, 4)
+
+if __name__ == '__main__':
+    main()
