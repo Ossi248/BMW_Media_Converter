b95cac49cf1f48348205e3b74e389d3027ca7b7d
LarsDerNorweger <boettger.colin@gmail.com>
2023-12-14 14:56:59 +0100

    implement abort

--- src/main.py
+++ src/main.py
@@ -5,10 +5,15 @@
 #
 
 import converter
-
+from time import sleep
 
 def done():
     print("DONE")
 
 
-converter.convert_directory_parallel("./test", "./hallo", done, print, 4)
+join,abort= converter.convert_directory_parallel("./test", "./hallo", done, print, 4)
+print("Hallo")
+sleep(5)
+abort()
+join()
+print("test")
